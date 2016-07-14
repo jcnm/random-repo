c52546af41c680af32b3f7848a23812bb1c1b390
J. Charles N. MBIADA <JCharles.NMBIADA@gmail.com>
2016-07-15 01:16:06 +0200

    Entities update

--- src/main/scala/model/entities/Airport.scala
+++ src/main/scala/model/entities/Airport.scala
@@ -3,18 +3,25 @@ package com.lunatech.qnr.model.entities
 
 /// Scala exclusive feature here, breaks Java compatibility
 final object AirportKind extends Enumeration{
-  val Closed, Heliport, SeaPlane, Small, Medium, Large, Balloon = Value
+  val Closed, Heliport, SeaPlaneBase, Small, Medium, Large, Balloon = Value
 }
 
 ///
 ///
-final class Airport(a_id: Int, a_name: String, a_ISO: String, a_kind: AirportKind.Value)
+final class Airport(a_id: Int,
+  a_name: String,
+  a_iso: String,
+  a_country: String,
+  a_municipality: String,
+  a_kind: AirportKind.Value)
     extends BaseEntity(a_id) {
   require(a_id > 0, "The airport ident should be greater than zero (0).")
 
-  val name = a_name
-  val kind = a_kind
-  val isoName = a_ISO
-
+  val name      = a_name
+  val kind      = a_kind
+  val isoName   = a_iso
+  val isoCountry    = a_country
+  val municipality  = a_municipality
+  
   override def toString() = s"Airport {id:$id, name:$name, kind:$kind, iso:$isoName}"
 }
--- src/main/scala/model/entities/Country.scala
+++ src/main/scala/model/entities/Country.scala
@@ -1,10 +1,12 @@
 package com.lunatech.qnr.model.entities
 
+
 ///
 ///
 final class Country(a_id: Int, a_name: String, a_code: String)
     extends BaseEntity(a_id) {
   require(a_id > 0, "The country ident should be greater than zero (0).")
+
   val name: String = a_name
   val code: String = a_code
 
