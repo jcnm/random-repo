6d852f0e57e2eb4bebe0c55f57ca202c58b96052
J. Charles N. MBIADA <JCharles.NMBIADA@gmail.com>
2016-07-21 00:20:23 +0200

    Main method takes in account the new refactoring

--- src/main/scala/Lunatech.scala
+++ src/main/scala/Lunatech.scala
@@ -6,14 +6,15 @@ package com.lunatech.qnr
 import com.lunatech.qnr.common._
 import com.lunatech.qnr.config._
 import com.lunatech.qnr.controller._
+import com.lunatech.qnr.model.ProxyFactoryDataSource
 import com.lunatech.qnr.view.QueryView
+import com.lunatech.qnr.model.entities.{Country, Airport, Runway}
 
 object Lunatech extends App {
 
   override def main(args: Array[String]) = {
 
-    var queries = QueryController.runwaysOf("fran")
-
+    val queries = QueryController.runwaysOf("fran")
     println(s"Please press [Q] for Quering and [P] for Reporting $QueryController")
     QueryView.showView(queries)
     // println(s"Countries Loaded : ${countriesProxy.size}")
