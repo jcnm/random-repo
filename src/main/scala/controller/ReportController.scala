9f79b2febe271c8b981a8a6d0fa8d2b20d802533
J. Charles N. MBIADA <JCharles.NMBIADA@gmail.com>
2016-07-21 00:21:01 +0200

    Add Report Controller

---
+++ src/main/scala/controller/ReportController.scala
@@ -0,0 +1,43 @@
+package com.lunatech.qnr.controller
+
+import com.lunatech.qnr.common._
+import com.lunatech.qnr.config._
+import com.lunatech.qnr.model.ProxyFactoryDataSource
+import com.lunatech.qnr.model.entities._
+import scala.util.{Try,Success,Failure}
+import com.lunatech.qnr.config._
+
+object ReportController {
+
+  var countriesProxy =
+    new ProxyFactoryDataSource[Country](Instance.dataOrigin, DataKind.Country)
+  var airportsProxy =
+    new ProxyFactoryDataSource[Airport](Instance.dataOrigin, DataKind.Airport)
+  var runwaysProxy =
+    new ProxyFactoryDataSource[Runway](Instance.dataOrigin, DataKind.Runway)
+
+  def initWith(countries: ProxyFactoryDataSource[Country] , airports:ProxyFactoryDataSource[Airport] , runways:ProxyFactoryDataSource[Runway]) = {
+    countriesProxy= countries
+    airportsProxy = airports
+    runwaysProxy  = runways
+  }
+
+  /*Naive way*/
+  def orderedAirportNumberCountry(limit: Int, f: ((Country, Int), (Country, Int)) => Boolean) {
+    val countriesWithAirportCount = countriesProxy.map( c => (c, airportsProxy.filter(a => a.isoCountry == c.code).size))
+    countriesWithAirportCount.toList.sortWith(f).take(limit)
+  }
+
+  def higherAirportNumberCountry(limit: Int) = {
+    orderedAirportNumberCountry(limit, (a: (Country, Int), b: (Country, Int)) => a._2 > a._2)
+  }
+
+
+  def lowerAirportNumberCountry(limit: Int) = {
+    orderedAirportNumberCountry(limit, (a: (Country, Int), b: (Country, Int)) => a._2 < a._2)
+  }
+
+  /* TODO use a optimized way using on or implicit object definition ?
+
+   */
+}
